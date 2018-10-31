e614390d65f5781a8b15386ed55c68e147adeb64
junster09 <ajr2z2025@gmail.com>
2018-10-31 15:46:25 -0700

    we did the psudocode

---
+++ HomeworkAssignment3/main.cpp
@@ -0,0 +1,39 @@
+/*
+print welcome group 1's calculator
+unsigned int Choice
+int numA
+int numB
+
+while(5 != Choice){
+printmenu()
+
+"What would you like to do: "
+input: Choice
+while (choice is not between 1-5){
+	"Invalid argument, reenter: "
+	input: Choice
+} 
+
+"please enter 2 numbers: "
+cin >> numA >> numB;
+
+switch(choice)
+case 1:
+	addition(numA, numB)
+	break
+case 2:
+	subtraction
+	break
+case 3:
+	multiplication
+	break
+case 4:
+	division
+	break
+case 5:
+	"thank u for using!"
+	break
+}
+
+
+*/
